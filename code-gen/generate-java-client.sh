74eaceebb8e395aea872d7f067fa46016c20d0c9
Justin Holmes <justinmichaelholmes@gmail.com>
2016-09-13 19:16:33 -0600

    supporting basic code gen

---
+++ code-gen/generate-java-client.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+if [ -e "swagger-codegen-cli.jar" ]
+then
+    echo 'swagger client is present, let us use it.'
+else
+    echo 'swagger client is not present, let us download it.'
+    wget http://repo1.maven.org/maven2/io/swagger/swagger-codegen-cli/2.2.1/swagger-codegen-cli-2.2.1.jar -O swagger-codegen-cli.jar
+fi
+
+java -jar swagger-codegen-cli.jar generate -l java -i ../swagger.yaml -c java-client-config.json -o target/java-client/
